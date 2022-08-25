daf1b6d7910143f7e3fb8f293af0a8a7aab997e7
frank-gich <gichuhi5781@gmail.com>
2022-08-25 12:29:19 +0300

    the Python function use impotrted files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,11 @@
 from add_0 import add
 
 
-def main():
+def add(a, b):
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    main()
+if __import__ == "__import__":
+    add()
+    
