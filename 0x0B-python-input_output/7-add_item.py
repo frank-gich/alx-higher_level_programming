0ac6e47742147e52d4c33870f4cbeaa52a1a8dad
frank-gich <gichuhi5781@gmail.com>
2022-12-09 01:21:25 +0300

    my changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Add item script."""
-import sys
+""" Load, add, save  """
+from sys import argv
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+""" Script that adds all arguments to a Python list,
+and then save them to a file """
 try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
-
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
+except Exception:
+    save_to_json_file(argv[1:], 'add_item.json')
