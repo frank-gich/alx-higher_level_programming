6f8264c7f505a5c7eb5d25b2aeb6e3d9ab5e9e30
frank-gich <gichuhi5781@gmail.com>
2022-12-09 00:55:00 +0300

    my changes

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+""" Student to JSON """
 
-"""Defines a Python class-to-JSON function."""
 
+class Student:
+    """ New class student """
 
-def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
