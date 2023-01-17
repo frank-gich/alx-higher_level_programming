d167cdc7d6db4a5208b1d68a7c75fecb7c7d9ff0
frank-gich <gichuhi5781@gmail.com>
2023-01-17 20:33:56 +0300

    javascript

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #  a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | awk '/Content-Length/ {print $2}'
+#!/bin/bash
+curl -sl "$1" | awk '/content-length/ {print $2}'
