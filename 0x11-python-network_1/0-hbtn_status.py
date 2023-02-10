8ffe4bb7ab03e055661fa19c8cdf21ec3e1f0cd7
frank-gich <gichuhi5781@gmail.com>
2023-02-11 02:19:52 +0300

    changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""script that fetches https://intranet.hbtn.io/status"""
+
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        r = response.read()
+
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        res = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf-8")))
