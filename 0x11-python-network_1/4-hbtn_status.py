393285b9abbe7da574028183fac5a324fccb7afe
frank-gich <gichuhi5781@gmail.com>
2023-02-11 02:05:53 +0300

    changes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
 
-    res = requests.get("https://intranet.hbtn.io/status")
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
