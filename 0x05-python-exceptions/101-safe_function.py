075333cb2e4523f21f8eb151853a79e0fb2f0fcb
frank-gich <gichuhi5781@gmail.com>
2023-02-11 00:13:55 +0300

    changes

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-from collections import UserDict
 import sys
 
 
 def safe_function(fct, *args):
-    """Executes a function safely.
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-    Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
-    """
     try:
-        result = fct(*args)
-        return (result)
-    except UserDict.DoesNotExist:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        func = fct(*args)
+
+    except Exception as error:
+        func = None
+        sys.stderr.write("Exception: {}\n".format(error))
+
+    return (func)
